87db93e7c7966eaa2676b28b98a60ac1fea43213
skckem <163977289+skckem@users.noreply.github.com>
2024-03-27 00:43:55 +0900

    2024-03-20 ossp 개인과제#2

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("Git의 세계에 오신 것을 환영합니다")
-print("이건 dev branch에서 작성한 내용입니다.")
+#include <stdio.h>
+int main()
+{
+        char x;
+double y;
+printf("이름을 입력하세요: ");
+scanf("%s", &x);
+printf("학번을 입력하세요: ");
+scanf("%lf", &y);
+printf("<출력>\n");
+printf("이름: %s",x);
+printf("학번: %lf",y);
+return 0;
